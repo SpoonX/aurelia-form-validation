84442acb0cbc235431f853410da88ea41d26b1fa
RWOverdijk <r.w.overdijk@gmail.com>
2017-02-17 11:45:31 +0100

    test(project): fix test

--- test/aurelia-skeleton-plugin.spec.js
+++ test/aurelia-skeleton-plugin.spec.js
@@ -1,12 +1,9 @@
 import {configure} from '../src/aurelia-form-validation';
-//import {Container} from 'aurelia-dependency-injection';
 
 describe('aurelia-form-validation', function() {
   describe('configure()', function() {
     it('Should call configure with a function', function() {
-      configure(()=>{
-        expect(true).toBe(true);
-      });
+      expect(typeof configure).toBe('function');
     });
   });
 });
